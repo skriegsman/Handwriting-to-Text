3975f799dc0f486b3b00ab782ddfca033a1f1c96
Sam Kriegsman <swkriegsman@gmail.com>
2019-02-16 17:22:29 -0500

    Fixed cropping error

--- findText-OpenCV(WIFI).py
+++ findText-OpenCV(WIFI).py
@@ -3,15 +3,13 @@ import pytesseract
 import numpy as np
 
 """ ---Initial Image Processing--- """
-image = cv2.imread("pictures/skew1.png", 0)
+image = cv2.imread("pictures/hp.png", 0)
 ret, thresh = cv2.threshold(image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-invert = cv2.bitwise_not(thresh)
-
-""" ---Blurs the image to remove noise and smooth edges--- """
-blur = cv2.GaussianBlur(invert, (7,7), 1)
+blur = cv2.GaussianBlur(thresh, (1,1), 0)
+invert = cv2.bitwise_not(blur)
 
 """ ---Auto Rotates the Image to Correct Skew--- """
-coords = np.column_stack(np.where(blur > 0))
+coords = np.column_stack(np.where(invert > 0))
 angle = cv2.minAreaRect(coords)[-1]
 
 if angle < -45: angle = -(90 + angle)
@@ -21,15 +19,15 @@ else: angle = -angle
 center = (w // 2, h // 2)
 M = cv2.getRotationMatrix2D(center, angle, 1.0)
 rotated = cv2.warpAffine(invert, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+reInvert = cv2.bitwise_not(rotated)
 
 
 """ ---Auto Crop Image using the above minAreaRect--- """
-points = np.argwhere(rotated==255)
+points = np.argwhere(reInvert==0)
 points = np.fliplr(points)
 x, y, w, h = cv2.boundingRect(points)
-x, y, w, h = x-10, y-10, w+20, h+20
-crop = rotated[y:y+h, x:x+w]
+crop = reInvert[y:y+h, x:x+w]
 
 """ ---Finds the text in the processed image--- """
-text = pytesseract.image_to_string(rotated, config= '--psm 1 --oem 1')
+text = pytesseract.image_to_string(crop, config='--psm 1 --oem 1')
 print(text)
