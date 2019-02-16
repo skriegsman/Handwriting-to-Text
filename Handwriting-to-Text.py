58a3e0c8d6abd7d510dc06674dc659068e4a5cfd
Sam Kriegsman <swkriegsman@gmail.com>
2019-02-15 23:18:01 -0500

    added example code

--- Handwriting-to-Text.py
+++ Handwriting-to-Text.py
@@ -1,16 +1,8 @@
-# https://www.pyimagesearch.com/2018/08/20/opencv-text-detection-east-text-detector/
-#https://www.pyimagesearch.com/2018/09/17/opencv-ocr-and-text-recognition-with-tesseract/
 import cv2
 import numpy as np
 
-#Constants
-minLetterSize = 20
-maxLetterSize = 200
-b, g, r = 255, 0, 255
-lineWieght = 1
-
 #load image in greyscale (0) under var name img
-originalImage = cv2.imread('ben.jpg')
+originalImage = cv2.imread('pictures/abcdef.png')
 workingImage = cv2.cvtColor(originalImage.copy(), cv2.COLOR_BGR2GRAY)
 
 #add adaptive thresholding  with a gaussian blur here to get rid of grays and make the page black and white only
@@ -23,8 +15,7 @@ workingImage = cv2.adaptiveThreshold(workingImage, 255, cv2.ADAPTIVE_THRESH_GAUS
 contours, ret = cv2.findContours(workingImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 for contour in contours: #For each contour
     [x, y, X, Y] = cv2.boundingRect(contour) # get rectangle bounding contour
-    if(X > minLetterSize and Y > minLetterSize and X < maxLetterSize and Y < maxLetterSize): # Don't plot small false positives or large boxes that contain text
-        cv2.rectangle(originalImage, (x, y), (x + X, y + Y), (b, g, r), lineWieght) # draw pink rectangle around contour on original image
+    cv2.rectangle(originalImage, (x, y), (x + X, y + Y), (255, 0, 255), 1) # draw pink rectangle around contour on original image
 
 #Displays the image seen by the computer for analysis
 cv2.imshow("Original Image", originalImage)
