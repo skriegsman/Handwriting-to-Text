204b72c81e4ca54e9a328a5c9237e97e0dbcddd0
Sam Kriegsman <swkriegsman@gmail.com>
2019-02-16 10:05:32 -0500

    misc

---
+++ findLetters-tesseract.py
@@ -0,0 +1,19 @@
+import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
+from textblob import TextBlob
+
+originalImage = Image.open("pictures\hp.png")
+workingImage = originalImage.copy()
+workingImage = workingImage.filter(ImageFilter.MedianFilter())
+
+enhanceSharpness = ImageEnhance.Sharpness(workingImage)
+enhanceColor = ImageEnhance.Color(workingImage)
+
+workingImage = enhanceSharpness.enhance(2)
+workingImage = enhanceColor.enhance(0)
+workingImage = workingImage.convert('1')
+
+text = pytesseract.image_to_string(workingImage)
+correctedText = TextBlob(text).correct()
+
+print(correctedText)
